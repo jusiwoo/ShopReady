231d13c77f19c9ba3a739c31f696d2b709da7c4a
jusiwoo <jusiwoo07@gmail.com>
2025-02-11 16:57:40 +0900

    add script

--- script/script.js
+++ script/script.js
@@ -1,1 +1,15 @@
-let 
+let addBtn = $('.add');
+let deleteBtn = $('.delete');
+let listContainer = $('.listContainer');
+
+addBtn.on('click',function(){
+    let li = $('li').first().clone(true);
+    let inp = $(this).prev().val();
+
+    listContainer.prepend(li);
+    li.children('span').text(inp);
+});
+
+deleteBtn.on('click',function(){
+    $(this).parent().remove();
+});
